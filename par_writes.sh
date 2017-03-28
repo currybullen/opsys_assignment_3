587a9ec5b1a7caf9574bfd95c7284bf60e665978
Mikael Karlsson <mikaelhakarlsson@gmail.com>
2017-03-28 22:07:17 +0200

    Changed name from seq_writes to par_writes

--- par_writes.sh
+++ par_writes.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+source settings.cfg
 total=0
 scheduler=$(./get_scheduler.sh)
-output=results/${scheduler}_${NUM_FILES}f_${SIZE}b_${NUM_THREADS}t
+output=$RESULTS_DIR/${scheduler}_${NUM_FILES}f_${SIZE}b_${NUM_THREADS}t
 > $output
 for i in $(seq 1 $1); do
     rm -rf generated/*
     start=$SECONDS
-    ./target/seq_writes.o $NUM_FILES $SIZE $NUM_THREADS generated 4096 >/dev/null 
+    ./target/par_writes $NUM_FILES $SIZE $NUM_THREADS $OUTPUT_DIR $BUF_SIZE >/dev/null 
     time=$((SECONDS-start))
     echo $time >> ${output}
     total=$((total+time))
